def68bc993d17d3739389cab8c987555dbd02f86
hampi <hampi@pc-00145.dueni.ch>
2014-01-08 00:15:48 +0100

    initial commit

---
+++ resource-contracts/src/main/java/ch/dueni/contracts/bean/ContractsBean.java
@@ -0,0 +1,25 @@
+package ch.dueni.contracts.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class ContractsBean implements Serializable {
+    
+    String contracts;
+
+    public String getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(String contracts) {
+    	if ("".equals(contracts)) {
+    		this.contracts = null;
+    	} else {
+    		this.contracts = contracts;
+    	}
+    }
+}
