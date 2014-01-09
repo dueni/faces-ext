d4c08913fffbbd42b811f48871c5db9f9eaf75ec
hampi <hampi@pc-00145.dueni.ch>
2014-01-10 00:09:45 +0100

    refactored to contracts in jars - except initial contract on webapp

---
+++ resource-contracts/contracts-web/src/main/java/ch/dueni/contracts/context/ContractsFacesContext.java
@@ -0,0 +1,73 @@
+package ch.dueni.contracts.context;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.el.ELContext;
+import javax.el.ELException;
+import javax.el.ExpressionFactory;
+import javax.el.ValueExpression;
+import javax.faces.context.FacesContext;
+import javax.faces.context.FacesContextWrapper;
+import javax.faces.event.PhaseId;
+
+/**
+ * Wrap FacesContext to allow general contracts activation through a managed
+ * bean.
+ * 
+ * @author dueni
+ * 
+ */
+public class ContractsFacesContext extends FacesContextWrapper {
+
+	private static final Logger LOG = Logger.getLogger(ContractsFacesContext.class.getName());
+
+	private static final String ACTIVE_CONTRACTS = "active-contracts";
+
+	private FacesContext wrapped;
+
+	private boolean activeContractsEvaluated = false;
+
+	public ContractsFacesContext(FacesContext toWrap) {
+		wrapped = toWrap;
+	}
+
+	@Override
+	public FacesContext getWrapped() {
+		return wrapped;
+	}
+
+	@Override
+	public List<String> getResourceLibraryContracts() {
+		if (!activeContractsEvaluated) {
+			activeContractsEvaluated = true;
+			String value = getExternalContext().getInitParameter(ACTIVE_CONTRACTS);
+			if (value != null) {
+				try {
+					ELContext el = getELContext();
+					ExpressionFactory elFactory = getApplication().getExpressionFactory();
+					ValueExpression ve = elFactory.createValueExpression(el, value, Object.class);
+					Object result = ve.getValue(el);
+					if (result instanceof String) {
+						String[] contracts = ((String) result).split(",");
+						getWrapped().setResourceLibraryContracts(Arrays.asList(contracts));
+					}
+				} catch (ELException elx) {
+					LOG.log(Level.SEVERE, "Exception while evaluating '" + ACTIVE_CONTRACTS
+							+ "' web.xml context-parameter!", elx);
+				}
+			}
+		}
+		return getWrapped().getResourceLibraryContracts();
+	}
+	
+	@Override
+	public void setCurrentPhaseId(PhaseId currentPhaseId) {
+		if (currentPhaseId == PhaseId.RENDER_RESPONSE) {
+			activeContractsEvaluated = false;
+		}
+		super.setCurrentPhaseId(currentPhaseId);
+	}
+}
---
+++ resource-contracts/contracts-web/src/main/java/ch/dueni/contracts/context/ContractsFacesContextFactory.java
@@ -0,0 +1,28 @@
+package ch.dueni.contracts.context;
+
+import javax.faces.FacesException;
+import javax.faces.context.FacesContext;
+import javax.faces.context.FacesContextFactory;
+import javax.faces.lifecycle.Lifecycle;
+
+public class ContractsFacesContextFactory extends FacesContextFactory {
+	
+	private FacesContextFactory wrapped;
+	
+	public ContractsFacesContextFactory(FacesContextFactory toWrap) {
+		wrapped = toWrap;
+	}
+
+	@Override
+	public FacesContext getFacesContext(Object context, Object request,
+			Object response, Lifecycle lifecycle) throws FacesException {
+		FacesContext jsf = getWrapped().getFacesContext(context, request, response, lifecycle);
+		return new ContractsFacesContext(jsf);
+	}
+	
+	@Override
+	public FacesContextFactory getWrapped() {
+		return wrapped;
+	}
+
+}
